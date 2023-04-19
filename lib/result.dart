5e516abbf3d853685eeb80396b2eefa8125cd675
seoyeon <seoyeonn051@gmail.com>
2023-04-20 00:38:14 +0900

    fix: CallBack Result 모델 추가

---
+++ lib/result.dart
@@ -0,0 +1,24 @@
+import 'package:camera/camera.dart';
+import 'package:test2/service/accelerometer_data_service.dart';
+
+class NetWorkResult {
+  Result result;
+  dynamic response;
+
+  NetWorkResult({
+    required this.result,
+    this.response,
+  });
+}
+
+enum Result {
+  fail,
+  success,
+}
+
+class CallBackResult {
+  List<Data> data;
+  String filePath;
+
+  CallBackResult({required this.data, required this.filePath});
+}
