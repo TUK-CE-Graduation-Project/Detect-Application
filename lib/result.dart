6278bf24838d28ed8ed0a1fd638eb69e52739a08
seoyeon <seoyeonn051@gmail.com>
2023-04-27 18:27:52 +0900

    fix: 정보 형식 서버로 보낼 수 있게 수정

--- lib/result.dart
+++ lib/result.dart
@@ -1,16 +1,27 @@
 import 'package:camera/camera.dart';
 import 'package:test2/service/accelerometer_data_service.dart';
 
-class NetWorkResult {
-  Result result;
+class PotholeRegistrationRequest {
+  int? geotabId;
+  int xacc;
+  int yacc;
+  int zacc;
+  Point point;
   dynamic response;
 
-  NetWorkResult({
-    required this.result,
-    this.response,
+  PotholeRegistrationRequest({
+    this.geotabId,
+    required this.xacc,
+    required this.yacc,
+    required this.zacc,
+    required this.point
   });
 }
 
+class Point {
+  late int x;
+  late int y;
+}
 enum Result {
   fail,
   success,
